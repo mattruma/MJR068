bbded1190e5c138c99666dad86cbfeb535efe88e
mattruma <mattruma@gmail.com>
2020-09-22 12:37:55 -0400

    initial commit

---
+++ src/FunctionApp1.Tests/PingFunctionApp.ps1
@@ -0,0 +1,34 @@
+Param(
+    [String] [Parameter(Mandatory = $true)] $FunctionUrl
+)
+
+$StatusCode = 0
+$NumberOfRetries = 3
+$Retries = 0
+$OkStatusCode = 200
+$RetryDelayInSeconds = 5
+
+Do 
+{
+    Write-Host ""
+    Write-Host "Pinging $($FunctionUrl)."
+
+    $StatusCode = (Invoke-WebRequest -URI $FunctionUrl).StatusCode
+    
+    Write-Host "Responded with status code $($StatusCode)." 
+
+    If ($StatusCode -ne $OkStatusCode) {
+        $Retries = $Retries + 1
+        If ($Retries -le $NumberOfRetries) {
+            Write-Host "Retrying $($Retries) of $($NumberOfRetries) and sleeping for $($RetryDelayInSeconds) second(s)." 
+            Start-Sleep -s $RetryDelayInSeconds
+        } Else {
+            Write-Host "Maximum retries exhausted."
+        }
+    }
+}    
+While ($StatusCode -ne $OkStatusCode -and $Retries -le $NumberOfRetries)
+
+If ($StatusCode -ne $OkStatusCode) {
+    Throw "Expected status code $($OkStatusCode) but received status code $($StatusCode)."
+}
